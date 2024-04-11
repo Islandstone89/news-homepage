930f6447883657aeb3ad743fc51fc6741f78167f
Øystein Håberg <islandstone89@gmail.com>
2024-04-12 00:02:21 +0200

    * Created js file and selected menu-btn * Fixed main grid, added meedia queries * Styled "new articles" and "other articles" * Added flow utility class

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/* Select menu button */
+let menuBtn = document.querySelector(".js-menu-btn");
+
+/* Add event listener */
+menuBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    alert("hi")
+})
