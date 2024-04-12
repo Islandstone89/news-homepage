d742bf7190fa5edc125f6884ed6c0f4d3104c8cf
Øystein Håberg <islandstone89@gmail.com>
2024-04-13 00:51:56 +0200

    * Added functionality for open and close menu buttons

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 
-/* Select menu button */
-let menuBtn = document.querySelector(".js-menu-btn");
+/* Select open menu button */
+let menuBtnOpen = document.querySelector(".js-menu-btn-open");
 
-/* Add event listener */
-menuBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    alert("hi")
+/* Select close menu button */
+let menuBtnClose = document.querySelector(".js-menu-btn-close");
+
+/* Add event listener on open menu button*/
+menuBtnOpen.addEventListener("click", (e) => {
+    menuBtnOpen.setAttribute("aria-expanded", "true")
+    menuBtnClose.setAttribute("aria-expanded", "true")
+})
+
+/* Add event listener on close menu button*/
+menuBtnClose.addEventListener("click", (e) => {
+    menuBtnOpen.setAttribute("aria-expanded", "false")
+    menuBtnClose.setAttribute("aria-expanded", "false")
 })
